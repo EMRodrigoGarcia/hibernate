5a03a7ded46a639a749a8082109a543a908217be
Rodrigo G. <rglopez300@gmail.com>
2021-01-11 16:32:18 +0100

    a

--- src/main/java/org/dam2/hibernate/App.java
+++ src/main/java/org/dam2/hibernate/App.java
@@ -1,10 +1,21 @@
 package org.dam2.hibernate;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+
 public class App {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		EntityManagerFactory factory = Persistence.createEntityManagerFactory("hibernate");
+		EntityManager manager = factory.createEntityManager();
+		
+		manager.getTransaction().begin();
+		
+		manager.getTransaction().commit();
+		
+		manager.close();
 	}
 
 }
